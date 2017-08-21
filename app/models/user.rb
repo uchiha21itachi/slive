d6976d713e0f45c3fa423c16312b1ac94a6e3a3e
Mathieu Jouffroy <mathieu.jouffroy@gmail.com>
2017-08-21 19:51:48 -0300

    removed user password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
 
   validates :full_name, presence: true
   validates :email, presence: true
-  validates :encrypted_password,
-            presence: true, confirmation: true, length: {minimum: 6},
-            on: :create
-  validates :encrypted_password, confirmation: true,
-            length: {minimum: 6},
-            on: :update
+  # validates :encrypted_password,
+  #           presence: true, confirmation: true, length: {minimum: 6},
+  #           on: :create
+  # validates :encrypted_password, confirmation: true,
+  #           length: {minimum: 6},
+  #           on: :update
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
