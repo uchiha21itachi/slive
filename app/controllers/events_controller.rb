6e2e399374500ed11054bd3d7399e3a1bc7ecd7b
Mathieu Jouffroy <mathieu.jouffroy@gmail.com>
2017-08-22 12:06:10 -0300

    CRUD events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,23 @@
 class EventsController < ApplicationController
 
-  before_action :set_event,  only: [:edit, :show, :update, :destroy]
+  before_action :set_event, only: [:edit, :show, :update, :destroy]
 
   def index
+    @events = Event.where(params[user: current_user])
+  end
+
+
+  def participated_events
     @events = Event.all
+    @your_events = []
+    @events.each do |event|
+      participants_events = @events.events_users
+      participants_events.each do |participant|
+        if participant == current_user
+          @your_events << event
+        end
+      end
+    end
   end
 
   def show
@@ -15,8 +29,9 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
+    @event.user = current_user
     if @event.save
-      redirect_to
+      redirect_to event_path(@event)
     else
       render :new
   end
@@ -25,24 +40,28 @@ class EventsController < ApplicationController
   end
 
   def update
+    @user =  current_user
     if @event.update(event_params)
-      redirect_to
+      redirect_to event_path(@event), notice: 'Event was successfully updated.'
     else
       render :edit
   end
 
   def destroy
     @event.destroy
-    redirect_to
+    redirect_to events_path, notice: 'Event was successfully deleted.'
   end
 
 private
 
   def event_params
-    params.require(:event).permit(:name, :date, :time, :description, :address, :token)
+    params.require(:event).permit(:name, :date, :time, :description, :address)
   end
 
   def set_event
     @event = Event.find(params[:id])
+  end
+
+  def creat_token
   end
 end
