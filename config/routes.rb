fe8c370e1ed5e1bb2fc552194170ee5d0a59ef60
diogo bahia <laranjaum@gmail.com>
2017-08-22 14:53:40 -0300

    user controler and views working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :events
   devise_for :users
+  resources :users, only: [:edit , :show, :update, :index]
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
