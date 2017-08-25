3ae57b4649b1f6a5b2fa7e5f983a0d180fd936d1
Mathieu Jouffroy <mathieu.jouffroy@gmail.com>
2017-08-25 16:04:25 -0300

    gemfilelock +dbschema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170824201227) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +58,37 @@ ActiveRecord::Schema.define(version: 20170824201227) do
     t.index ["user_id"], name: "index_events_users_on_user_id", using: :btree
   end
 
+  create_table "fae_users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "role_id"
+    t.boolean  "active"
+    t.string   "language"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["confirmation_token"], name: "index_fae_users_on_confirmation_token", unique: true, using: :btree
+    t.index ["email"], name: "index_fae_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_fae_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["role_id"], name: "index_fae_users_on_role_id", using: :btree
+    t.index ["unlock_token"], name: "index_fae_users_on_unlock_token", unique: true, using: :btree
+  end
 
   create_table "livemessages", force: :cascade do |t|
     t.string   "description"
