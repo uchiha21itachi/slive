e816a402ad8b6c398023eb1b0646c2ebdac693c3
yash sharma <yash221b@gmail.com>
2017-08-31 16:35:05 -0300

    fixing the particle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 
 
 
-particlesJS.load('particles-js', '/assets/particles.json', function() {
+particlesJS.load('particles-js', '/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
