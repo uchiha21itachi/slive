980177ef820e7e69a4787cd1db12ad435d645e18
yash sharma <yash221b@gmail.com>
2017-09-01 14:17:35 -0300

    finally finished the question and answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -26,6 +26,7 @@ class QuestionsController < ApplicationController
     if @question.save
       respond_to do |format|
         html = render_to_string partial: "show_question", locals: { question: @question, answer: Answer.new }
+        p html
         Pusher.trigger("event-#{@event.token}", 'question', {
           question: @question.question,
           question_html: html,
