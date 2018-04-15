25d2ae4f5838ae1df50130bac8ef2c217d1f9dc1
yash sharma <yash221b@gmail.com>
2018-04-15 12:15:25 +0530

    Added schema

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,13 +1,8 @@
 class Survey < ApplicationRecord
 	belongs_to :user
 	belongs_to :event
-
 	validates_presence_of :user, :event
-
-   
-    has_many :options, inverse_of: :survey
-    accepts_nested_attributes_for :options, reject_if: :all_blank, allow_destroy: true
-    has_many :votes
-
-    
+	has_many :options, inverse_of: :survey
+  accepts_nested_attributes_for :options, reject_if: :all_blank, allow_destroy: true
+  has_many :votes
 end
